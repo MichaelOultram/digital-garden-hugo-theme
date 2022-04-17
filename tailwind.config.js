4d4a4464ee5dc7e94bca550d5ef9055c0e36cb03
Michael Oultram <michael@oultr.am>
2022-04-17 15:34:45 +0100

    Display "No items" when there is nothing to show

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,7 @@
 module.exports = {
   content: [
-    "./content/**/*.md",
-    "./content/**/*.html",
     "./layouts/**/*.html",
     "./assets/**/*.js",
-    "./themes/digitalgarden/content/**/*.md",
-    "./themes/digitalgarden/content/**/*.html",
-    "./themes/digitalgarden/layouts/**/*.html",
-    "./themes/digitalgarden/assets/**/*.js",
   ],
   darkMode: 'class',
   theme: {
